e6574c6b50cbe7ec1b9d0da317b80ee31845cd6e
sadforver <1123195628@qq.com>
2021-08-05 12:53:54 +0800

    精简代码

--- app/views.py
+++ app/views.py
@@ -1,9 +1,4 @@
-import json
-import datetime
-from django.http import HttpResponse
 from rest_framework.response import Response
-
-from .serialize import DateEncoder
 from rest_framework.views import APIView
 from studinfo.rawsql import Sqlca
 
@@ -13,108 +8,98 @@ class UserView(APIView):
     def get(self, request ):
         try:
             results=request.GET.get('results')
-            print(type(results))
             page=request.GET.get('page')
-            print(type(page))
             product=int(results)*(int(page)-1)
-            print(product)
             gender = request.GET.get('gender')
             studentType = request.GET.get('studentType')
-            print(gender)
-            print(studentType)
             sort=request.GET.get('sortField')
             order=request.GET.get('sortOrder')
-            print(sort)
             if studentType !='null' and studentType :
-                print("000")
                 if gender!='null' and gender:
-                    print("step1")
                     if sort == 'studentId':
                         if order == 'ascend':
-                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s and gender=%s and "studentId" not in (select "studentId" from student where "studentType"= %s and gender=%s order by "studentId" limit %s) order by "studentId" limit %s '
-                            data = Sqlca.execute(sql,[studentType, gender, studentType, gender, product, results])
+                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s and gender=%s  order by "studentId" limit %s offset %s'
+                            data = Sqlca.execute(sql,[studentType, gender,results,product])
                         else:
-                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s and gender=%s and "studentId" not in (select "studentId" from student where "studentType"= %s and gender=%s order by "studentId" desc limit %s) order by "studentId" desc limit %s '
-                            data = Sqlca.execute(sql,[studentType, gender, studentType, gender, product, results])
+                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s and gender=%s order by "studentId" desc limit %s offset %s'
+                            data = Sqlca.execute(sql,[studentType, gender,results,product])
                     elif sort == 'schoolYear':
                         if order == 'ascend':
-                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s and gender=%s and "studentId" not in (select "studentId" from student where "studentType"= %s and gender=%s order by "schoolYear" limit %s) order by "schoolYear" limit %s '
-                            data = Sqlca.execute(sql, [studentType, gender, studentType, gender, product, results])
+                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s and gender=%s order by "schoolYear" limit %s offset %s'
+                            data = Sqlca.execute(sql, [studentType, gender,results,product])
                         else:
-                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s and gender=%s and "studentId" not in (select "studentId" from student where "studentType"= %s and gender=%s order by "schoolYear" desc limit %s) order by "schoolYear" desc limit %s '
-                            data = Sqlca.execute(sql, [studentType, gender, studentType, gender, product, results])
+                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s and gender=%s order by "schoolYear" desc limit %s offset %s'
+                            data = Sqlca.execute(sql, [studentType, gender,results,product])
                     else:
-                        sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s and gender=%s and "studentId" not in (select "studentId" from student where "studentType"= %s and gender=%s limit %s) limit %s '
-                        data = Sqlca.execute(sql, [studentType, gender,studentType, gender,product,results])
+                        sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s and gender=%s limit %s offset %s'
+                        data = Sqlca.execute(sql, [studentType, gender,results,product])
                     sql='select count(gender) from student  where "studentType"= %s and gender=%s '
                     count=Sqlca.execute(sql,[studentType, gender])[0]['count']
-                    print(count)
                 else:
                     if sort=="studentId":
                         if order=="ascend":
-                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s  and "studentId" not in (select "studentId" from student where "studentType"= %s order by "studentId" limit %s)order by "studentId" limit %s  '
-                            data = Sqlca.execute(sql, [studentType, studentType, product, results])
+                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s order by "studentId" limit %s offset %s '
+                            data = Sqlca.execute(sql, [studentType,results,product])
                         else:
-                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s  and "studentId" not in (select "studentId" from student where "studentType"= %s order by "studentId" desc limit %s)order by "studentId" desc limit %s  '
-                            data = Sqlca.execute(sql, [studentType, studentType, product, results])
+                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s order by "studentId" desc limit %s offset %s '
+                            data = Sqlca.execute(sql, [studentType,results,product])
                     elif sort=="schoolYear":
                         if order=="ascend":
-                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s  and "studentId" not in (select "studentId" from student where "studentType"= %s order by "schoolYear" limit %s)order by "schoolYear" limit %s  '
-                            data = Sqlca.execute(sql, [studentType, studentType, product, results])
+                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s order by "schoolYear" limit %s offset %s '
+                            data = Sqlca.execute(sql, [studentType,results,product])
                         else:
-                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s  and "studentId" not in (select "studentId" from student where "studentType"= %s order by "schoolYear" desc limit %s)order by "schoolYear" desc limit %s  '
-                            data = Sqlca.execute(sql, [studentType, studentType, product, results])
+                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s  order by "schoolYear" desc limit %s offset %s '
+                            data = Sqlca.execute(sql, [studentType,results,product])
                     else:
-                        sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s  and "studentId" not in (select "studentId" from student where "studentType"= %s  limit %s) limit %s  '
-                        data = Sqlca.execute(sql, [studentType,studentType, product,results])
+                        sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where "studentType"= %s limit %s  offset %s'
+                        data = Sqlca.execute(sql, [studentType,results,product])
                     sql = 'select count(gender) from student  where "studentType"= %s '
                     count = Sqlca.execute(sql, [studentType])[0]['count']
             else:
                 if gender!='null' and gender:
                     if sort=="studentId":
                         if order=="ascend":
-                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where gender= %s and "studentId" not in (select "studentId" from student where gender= %s order by "studentId" limit %s)order by "studentId" limit %s '
-                            data = Sqlca.execute(sql, [gender, gender, product, results])
+                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where gender= %s order by "studentId" limit %s offset %s'
+                            data = Sqlca.execute(sql, [gender,results,product])
                         else:
-                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where gender= %s and "studentId" not in (select "studentId" from student where gender= %s order by "studentId" desc limit %s)order by "studentId" desc limit %s '
-                            data = Sqlca.execute(sql, [gender, gender, product, results])
+                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where gender= %s order by "studentId" desc limit %s offset %s'
+                            data = Sqlca.execute(sql, [gender, results,product])
                     elif sort=="schoolYear":
                         if order=="ascend":
-                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where gender= %s and "studentId" not in (select "studentId" from student where gender= %s order by "schoolYear" limit %s)order by "schoolYear" limit %s '
-                            data = Sqlca.execute(sql, [gender, gender, product, results])
+                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where gender= %s order by "schoolYear" limit %s offset %s'
+                            data = Sqlca.execute(sql, [gender, results,product])
                         else:
-                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where gender= %s and "studentId" not in (select "studentId" from student where gender= %s order by "schoolYear" desc limit %s)order by "schoolYear" desc limit %s '
-                            data = Sqlca.execute(sql, [gender, gender, product, results])
+                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where gender= %s order by "schoolYear" desc limit %s offset %s'
+                            data = Sqlca.execute(sql, [gender, results,product])
                     else:
-                        sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where gender= %s and "studentId" not in (select "studentId" from student where gender= %s  limit %s) limit %s '
-                        data = Sqlca.execute(sql, [gender,gender,product,results])
+                        sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo" from student where gender= %s limit %s offset %s'
+                        data = Sqlca.execute(sql, [gender,results,product])
                     sql = 'select count(gender) from student  where  gender=%s '
                     count = Sqlca.execute(sql, [ gender])[0]['count']
                 else:
                     if sort=="studentId":
                         if order=="ascend":
-                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo"  from student where "studentId" not in (select "studentId" from student order by "studentId" limit %s) order by "studentId" limit %s '
-                            data = Sqlca.execute(sql, [product, results])
+                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo"  from student order by "studentId" limit %s offset %s'
+                            data = Sqlca.execute(sql, [results,product])
                         else:
-                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo"  from student where "studentId" not in (select "studentId" from student order by "studentId" desc limit %s) order by "studentId" desc limit %s '
-                            data = Sqlca.execute(sql, [product, results])
+                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo"  from student  order by "studentId" desc limit %s offset %s'
+                            data = Sqlca.execute(sql, [results,product])
                     elif sort=="schoolYear":
                         if order=="ascend":
-                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo"  from student where "studentId" not in (select "studentId" from student order by "schoolYear" limit %s) order by "schoolYear" limit %s '
-                            data = Sqlca.execute(sql, [product, results])
+                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo"  from student order by "schoolYear" limit %s offset %s '
+                            data = Sqlca.execute(sql, [results,product])
                         else:
-                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo"  from student where "studentId" not in (select "studentId" from student order by "schoolYear" desc limit %s) order by "schoolYear" desc limit %s '
-                            data = Sqlca.execute(sql, [product, results])
+                            sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo"  from student order by "schoolYear" desc limit %s offset %s'
+                            data = Sqlca.execute(sql, [results,product])
                     else:
-                        sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo"  from student where "studentId" not in (select "studentId" from student  limit %s)  limit %s '
-                        data = Sqlca.execute(sql, [product, results])
+                        sql = 'select "studentId","studentName",gender,"schoolYear",telephone,email,"studentType","idNo"  from student limit %s offset %s '
+                        data = Sqlca.execute(sql, [results,product])
                     sql = 'select count(gender) from student'
                     count = Sqlca.execute(sql, [])[0]['count']
-            # columselected=count[0]['count']
-
             rtn={'code':1000,'message':'获取成功','data':data,'count':count}
         except Exception as e:
             rtn = {'code': 1001, 'message': '获取失败', 'data': {}}
+
         return Response(rtn)
     def post(self,request):
         data=request.data
